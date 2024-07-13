ff072f47387737e4481138dd3b0bd329fc8dc082
osparking <jbpark03@gmail.com>
2024-07-13 15:52:38 +0900

    시험 메소드 실행 전에 시험용 자료 미리 생성

--- src/test/java/space/bum/sboot/entity/ShoppingTest.java
+++ src/test/java/space/bum/sboot/entity/ShoppingTest.java
@@ -7,34 +7,44 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.hibernate.Session;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 
 @SpringBootTest
 class ShoppingTest {
   @Autowired
   private Session session;
-
-  @Test
-  @Transactional
-  void whenCartOfOneItemSavedAndRead_thenOneItemFoundInIt_then_Ok() {
+  
+  private long cartId = 0;
+  private Item item = null;
+  
+  @BeforeEach
+  void makeSomeTestData() {
     Cart costcoCart = new Cart();
     session.persist(costcoCart);
+    cartId = costcoCart.getCart_id();
     
     Item eggBox = new Item();
+    item = eggBox;
     eggBox.setCart(costcoCart);
     session.persist(eggBox);
     
     Set<Item> items = new HashSet<>();
     items.add(eggBox);
-    costcoCart.setItems(items);
-    
-    var cartRead = session.find(Cart.class, costcoCart.getCart_id());
+    costcoCart.setItems(items);    
+  }
+
+  @Test
+  @Transactional
+  void whenCartOfOneItemSavedAndRead_thenOneItemFoundInIt_then_Ok() {    
+    var cartRead = session.find(Cart.class, cartId);
     var itemsRead = cartRead.getItems();
     assertEquals(1, itemsRead.size()); 
-    assertTrue(itemsRead.contains(eggBox));
+    assertTrue(itemsRead.contains(item));
   }
 }
