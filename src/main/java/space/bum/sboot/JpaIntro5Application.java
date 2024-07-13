cd2a0e2d7d85a61eb15484ffabaad915ac027754
osparking <jbpark03@gmail.com>
2024-07-13 12:15:20 +0900

    1-to-many jpa 예제 개발 시작

---
+++ src/main/java/space/bum/sboot/JpaIntro5Application.java
@@ -0,0 +1,13 @@
+package space.bum.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaIntro5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaIntro5Application.class, args);
+	}
+
+}
---
+++ src/test/java/space/bum/sboot/JpaIntro5ApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum.sboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaIntro5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
