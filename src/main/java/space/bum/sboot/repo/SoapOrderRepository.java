1ab9888b457114330efbe3df7e3b0851284edc70
osparking <jbpark03@gmail.com>
2024-07-14 10:38:24 +0900

    저장소 메소드 - 정렬 증가/감소순 반영

--- src/main/java/space/bum/sboot/repo/SoapOrderRepository.java
+++ src/main/java/space/bum/sboot/repo/SoapOrderRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
+import space.bum.sboot.config.Direction;
 import space.bum.sboot.sort.SoapOrder;
 
 @Repository
@@ -14,11 +15,19 @@ public class SoapOrderRepository {
   @Autowired
   private EntityManager em;
 
-  public List<SoapOrder> getSortedSoapOrders() {
+  public List<SoapOrder> getSortedSoapOrders(Direction direction) {
 
     String jql = "Select so from SoapOrder as so order by so.orderId";
-    Query sortedSoapQuery = em.createQuery(jql, SoapOrder.class);
+    StringBuffer jsql = new StringBuffer();
+
+    jsql.append(jql);
+    if (direction == Direction.DESC) {
+      jsql.append(" desc");
+    }
+
+    Query sortedSoapQuery = em.createQuery(jsql.toString(), SoapOrder.class);
     List<SoapOrder> results = sortedSoapQuery.getResultList();
+
     return results;
   }
 }
