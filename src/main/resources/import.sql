befcffa8b436cc2c2c6928048a40383d75429baf
osparking <jbpark03@gmail.com>
2024-07-14 12:58:37 +0900

    두 레코드에 고객명 저장, 하나는 null

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT) values(1, 5);
-insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT) values(2, 10);
+insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT, CUSTOMER) values(1, 5, '최영미');
+insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT, CUSTOMER) values(2, 10, '김정국');
 insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT) values(3, 5);
