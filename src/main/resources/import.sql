9a423f8b814b77ecff8f64fb2756b723f8a1aeba
osparking <jbpark03@gmail.com>
2024-07-14 21:23:55 +0900

    내포된 개체 이중 정렬 코스 실습용 자료 조합

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,9 @@
-insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT, CUSTOMER) values(1, 5, '최영미');
-insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT, CUSTOMER) values(2, 10, '김정국');
-insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT) values(3, 5);
+insert into CUSTOMER(ID, CUST_NAME) VALUES (100, '최영미');
+insert into CUSTOMER(ID, CUST_NAME) VALUES (101, '김정국');
+insert into CUSTOMER(ID, CUST_NAME) VALUES (102, '안성기');
+
+insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT, CUSTOMER_ID) values(1, 5, 100);
+insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT, CUSTOMER_ID) values(3, 10, 100);
+insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT, CUSTOMER_ID) values(2, 3, 101);
+insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT, CUSTOMER_ID) values(4, 7, 101);
+insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT, CUSTOMER_ID) values(5, 1, 101);
