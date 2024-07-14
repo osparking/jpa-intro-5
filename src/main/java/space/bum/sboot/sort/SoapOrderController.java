0bd9f7cd98063d7f13dac2faf4cee61558f26821
osparking <jbpark03@gmail.com>
2024-07-14 10:39:01 +0900

    제어기 메소드 - 정렬 증가/감소 명령 하달

--- src/main/java/space/bum/sboot/sort/SoapOrderController.java
+++ src/main/java/space/bum/sboot/sort/SoapOrderController.java
@@ -4,20 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import space.bum.sboot.config.Direction;
 import space.bum.sboot.repo.SoapOrderRepository;
 
 @RestController
 @RequestMapping("/sortby")
 public class SoapOrderController {
-  
+
   @Autowired
   private SoapOrderRepository soRepo;
-  
-  @GetMapping("/id")
-  public List<SoapOrder > findOrderedSoapOrders() {
-    return soRepo.getSortedSoapOrders();
+
+  @GetMapping("/id/{direction}")
+  public List<SoapOrder> findOrderedSoapOrders(
+      @PathVariable(name = "direction") String direction) {
+    if ("desc".equalsIgnoreCase(direction)) {
+      return soRepo.getSortedSoapOrders(Direction.DESC);
+    } else {
+      return soRepo.getSortedSoapOrders(Direction.ASC);
+    }
+    
   }
 }
