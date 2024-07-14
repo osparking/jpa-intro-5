eecaff55c271a26e47d4bfb71d8ae601d007ddf2
osparking <jbpark03@gmail.com>
2024-07-14 12:57:59 +0900

    null 값이 허용된 멤버 비누 주문 개체에 추가

--- src/main/java/space/bum/sboot/sort/SoapOrder.java
+++ src/main/java/space/bum/sboot/sort/SoapOrder.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -26,4 +27,6 @@ public class SoapOrder {
   @ColumnDefault("CURRENT_TIMESTAMP")
   private LocalDateTime orderTime;
   private int soapCount;
+  @Column(nullable = true)
+  private String customer;
 }
