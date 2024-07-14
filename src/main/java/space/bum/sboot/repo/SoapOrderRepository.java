8027660a5325b4f1f9db1fab5835733cf46c32c1
osparking <jbpark03@gmail.com>
2024-07-14 12:00:27 +0900

    비누 주문 수량, 아이디 증가/감소순 - 저장소 메소드

--- src/main/java/space/bum/sboot/repo/SoapOrderRepository.java
+++ src/main/java/space/bum/sboot/repo/SoapOrderRepository.java
@@ -30,4 +30,20 @@ public class SoapOrderRepository {
 
     return results;
   }
+
+  public List<SoapOrder> getOrderSortByCount(Direction direction) {
+    String jql = "Select so from SoapOrder as so "
+        + "order by so.soapCount, so.orderId";
+    StringBuffer jsql = new StringBuffer();
+
+    jsql.append(jql);
+    if (direction == Direction.DESC) {
+      jsql.append(" desc");
+    }
+
+    Query sortedSoapQuery = em.createQuery(jsql.toString(), SoapOrder.class);
+    List<SoapOrder> results = sortedSoapQuery.getResultList();
+
+    return results;
+  }
 }
