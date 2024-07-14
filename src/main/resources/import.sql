aac73b9c0e240db6f9f8707c77c8e42ff1ecaa07
osparking <jbpark03@gmail.com>
2024-07-14 12:01:54 +0900

    정렬 효과 확인용 자료 수정 - 같은 수량 주문 2건 마련

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT) values(1, 10);
-insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT) values(2, 3);
+insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT) values(1, 5);
+insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT) values(2, 10);
 insert into SOAP_ORDER(ORDER_ID, SOAP_COUNT) values(3, 5);
