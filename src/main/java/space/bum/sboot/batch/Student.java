66fa4bc87ee71cd68d56a953cc0206be990191b4
osparking <jbpark03@gmail.com>
2024-07-15 12:12:01 +0900

    학생 - 학교 시스템의 둘째 개체

--- src/main/java/space/bum/sboot/batch/Student.java
+++ src/main/java/space/bum/sboot/batch/Student.java
@@ -1,5 +1,22 @@
 package space.bum.sboot.batch;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
 public class Student {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private long id;
+  
+  private String name;
+  
+  @ManyToOne
+  private School school;
 
 }
